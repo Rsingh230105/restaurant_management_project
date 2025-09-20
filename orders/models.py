483814dd5ef415e136cfef144e3308248b214bf6
Rsingh230105 <rajendra.singh@cdgi.edu.in>
2025-09-20 09:33:39 +0530

    49172

--- orders/models.py
+++ orders/models.py
@@ -6,7 +6,7 @@ class OrderStatus(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+        return f"OrderStatus {self.id} - {self.name}"
 
 class Order(models.Model):
     status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True,related_name='orders')
