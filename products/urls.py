c0b9a99b6b9403510d080e4e877e1e150dca3535
Rsingh230105 <rajendra.singh@cdgi.edu.in>
2025-09-22 18:16:21 +0530

    49971

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import *
+from .views import ItemView
+
+#Viewset ke liye as_view dictionary 
+item_list = ItemView.as_view({
+    'get':'list',    #GET -> list items(with search +pagination)
+    'post':'create', #POST ->create new item
+    'put':'update',  #put ->update item by ID
+})
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
+    path('items/<int:pk>/',item_list,name='item-update'),
 ]
