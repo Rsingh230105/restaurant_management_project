f78e3cc33d6a8bf78a0882b721aaec42065cf02e
Rsingh230105 <rajendra.singh@cdgi.edu.in>
2025-09-22 18:11:31 +0530

    49971

--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,9 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+    
+    def validate_item_price(self, value):
+        """ Ensure price is a positive number"""
+        if value <0 :
+            raise serializers.ValidationError("Price must be positive.")
+        return value
